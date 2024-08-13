dccdfeb931ce5466d6a047a2a74266bf5301bf75
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-13 12:26:26 -0500

    added Header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Header from "./components/Header";
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Header />
       <Footer />
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,17 @@
+import Logo from "./Logo";
+import Search from "./Search";
+import Switcher from "./Switcher";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="bg-primary p-3">
+      <div className="flex items-center justify-between max-w-[1400px] mx-auto">
+        <Logo />
+        <Search />
+        <Switcher />
+      </div>
+    </div>
+  );
 };
 
 export default Header;

b54c6e785b8776500ebe6b9de79f06b48f9c4797
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-13 11:45:23 -0500

    added Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
-function App() {
-  return <p>test</p>;
-}
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Header />
+      <Footer />
+    </>
+  );
+};
 
 export default App;

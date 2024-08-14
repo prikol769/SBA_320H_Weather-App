49f7930b1546a92e6da0f65007907b51a4c3fc3e
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-13 20:24:59 -0500

    added WeatherList compoent

---
+++ src/components/WeatherList.jsx
@@ -0,0 +1,21 @@
+import { formatTimestampToReadableDate } from "../utils/DateUtils";
+import WeatherItem from "./WeatherItem";
+
+const WeatherList = ({ dailyWeather }) => {
+  console.log(dailyWeather, "dailyWeather");
+
+  return (
+    <div>
+      <div className="py-4 border-y border-[#dedede]">
+        <h2 className="text-xl font-semibold">
+          {formatTimestampToReadableDate(dailyWeather[0].dt)}
+        </h2>
+        {dailyWeather.map((hourlyWeather, index) => (
+          <WeatherItem key={index} hourlyWeather={hourlyWeather} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default WeatherList;

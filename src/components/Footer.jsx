b4c4782483efe71e9ffba8c2b760e4f21cb62e90
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-13 20:24:13 -0500

    added Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-[#00354f] text-white p-4 text-center">
+      © Weather, LLC 2024, Mykyta Krumalenko
+    </div>
+  );
 };
 
 export default Footer;
